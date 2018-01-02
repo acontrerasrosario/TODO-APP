dd29409c02d105e3fb8070c7c14e4a8b2c0008a0
andrea14polanco <alpm9614@hotmail.com>
2018-01-02 15:43:13 -0400

    Primera version del tab de Tags

--- TodoApp-MovileApp/www/js/Controllers/NotesGroupsCtrl.js
+++ TodoApp-MovileApp/www/js/Controllers/NotesGroupsCtrl.js
@@ -3,8 +3,26 @@
         // Addig the  controller function
         // to the context module
         .controller('NotesGroupsCtrl', NotesGroupsCtrl);
-    NotesGroupsCtrl.$inject = ['$scope', 'AboutUsServ'];
-    function NotesGroupsCtrl($scope, people) {
+    NotesGroupsCtrl.$inject = ['$scope', 'AboutUsServ', '$ionicModal'];
+    function NotesGroupsCtrl($scope, people,$ionicModal) {
             $scope.message = "work";
+
+            $scope.newTag = {
+                "tagName":null,
+                "description": null
+            }
+    
+      $ionicModal.fromTemplateUrl('templates/modalGroup.html', {
+        scope: $scope,
+        animation: 'slide-in-up'
+      }).then(function(modal) {
+        $scope.modal = modal;
+      });
+      $scope.openModal = function() {
+        $scope.modal.show();
+      };
+      $scope.closeModal = function() {
+        $scope.modal.hide();
+      };
 
     }
